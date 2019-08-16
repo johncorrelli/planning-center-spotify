57a24a300d5b6944d25b2785676efd831e0e07b5
John <john@teamgantt.com>
2019-08-15 21:44:19 -0400

    Runs sync for all service types

--- app/Models/PlanningCenter/PlanningCenter.php
+++ app/Models/PlanningCenter/PlanningCenter.php
@@ -3,15 +3,9 @@
 namespace App\Models\PlanningCenter;
 
 use App\Models\Api;
-use DateTimeImmutable;
 
 class PlanningCenter
 {
-    /**
-     * int The number of future services to sync on a given run.
-     */
-    const NUM_SERVICES = 4;
-
     /**
      * An instance of the API object used to connect to Planning Center.
      *
@@ -34,23 +28,14 @@ class PlanningCenter
     private $secretKey;
 
     /**
-     * The id of your service type in Planning Center.
-     *
-     * @var int
-     */
-    private $serviceTypeId;
-
-    /**
      * @param string $applicationId
      * @param string $secretKey
-     * @param int $serviceTypeId
      * @param Api $api
      */
-    public function __construct(string $applicationId, string $secretKey, int $serviceTypeId, Api $api)
+    public function __construct(string $applicationId, string $secretKey, Api $api)
     {
         $this->applicationId = $applicationId;
         $this->secretKey = $secretKey;
-        $this->serviceTypeId = $serviceTypeId;
         $this->api = $api;
 
         $authorization = base64_encode("{$applicationId}:{$secretKey}");
@@ -59,25 +44,19 @@ class PlanningCenter
     }
 
     /**
-     * Returns upcoming ServicePlans.
-     *
-     * @param DateTimeImmutable $servicesAfterDate Returns services after this date
+     * Returns the ServiceTypes.
      *
      * @return array
      */
-    public function getServicePlansAfter(DateTimeImmutable $servicesAfterDate): array
+    public function getServiceTypes(): array
     {
-        $after = $servicesAfterDate->format('Y-m-d');
-        $numServices = self::NUM_SERVICES;
-        $serviceTypeId = $this->serviceTypeId;
-
-        $services = $this->api->request(
+        $serviceTypes = $this->api->request(
             'GET',
-            "service_types/{$this->serviceTypeId}/plans?filter=after&after={$after}&per_page={$numServices}"
+            'service_types'
         )->data;
 
-        return array_map(function ($service) use ($serviceTypeId) {
-            return new ServicePlan($serviceTypeId, $service, $this->api);
-        }, $services);
+        return array_map(function ($serviceType) {
+            return new ServiceType($serviceType, $this->api);
+        }, $serviceTypes);
     }
 }
